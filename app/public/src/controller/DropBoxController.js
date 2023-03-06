fe6e85cd4bd236ad70fc3797ee32313fb900e908
Ryan Santana <ryan.santana.clear@gmail.com>
2023-03-06 12:27:02 -0300

    Feat: Implementing file rename functionality

--- app/public/src/controller/DropBoxController.js
+++ app/public/src/controller/DropBoxController.js
@@ -50,6 +50,24 @@ class DropBoxController {
 
     initEvents() {
         
+        this.btnRename.addEventListener('click', e=>{
+
+            let li = this.getSelection()[0];
+
+            let file = JSON.parse(li.dataset.file);
+
+            let name = prompt("Renomear o arquivo:", file.originalFilename);
+
+            if (name) {
+
+                file.originalFilename = name;
+
+                this.getFirebaseRef().child(li.dataset.key).set(file);
+
+            }
+
+        });
+
         this.listFilesEl.addEventListener('selectionchange', e=>{
 
             switch (this.getSelection().length) {
@@ -375,6 +393,7 @@ uploadComplete(){
         let li = document.createElement('li');
 
         li.dataset.key = key;
+        li.dataset.file = JSON.stringify(file);
 
         li.innerHTML = `
           <li>
