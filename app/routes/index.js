53a5c7b50af5fd14596bb4777551da33080559d2
Ryan Santana <ryan.santana.clear@gmail.com>
2023-03-07 12:27:18 -0300

    Feat: Implementing the functionality of opening files

--- app/routes/index.js
+++ app/routes/index.js
@@ -8,6 +8,39 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get ('/file', (req, res)=>{
+
+  let path = './' + req.query.path;
+
+  if (fs.existsSync(path)) {
+
+    fs.readFile(path, (err, data)=>{
+
+      if (err) {
+
+        console.error(err);
+        res.status(400).json({
+          error: err
+        });
+
+      } else {
+
+        res.status(200).end(data);
+
+      }
+
+    });
+
+  } else {
+
+    res.status(404).json({
+      error:'FIles not found.'
+    });
+
+  }
+
+});
+
 router.delete('/file', (req, res)=>{
 
   let form = new formidable.IncomingForm({
@@ -35,6 +68,12 @@ router.delete('/file', (req, res)=>{
 
       });
 
+    } else {
+
+      res.status(404).json({
+        error:'FIles not found.'
+      });
+  
     }
 
   });
