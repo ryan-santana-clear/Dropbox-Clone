5e7f8737d235f06e2760bebc1c50aefa57ef0d80
Ryan Santana <ryan.santana.clear@gmail.com>
2023-03-08 11:13:50 -0300

    Feat: Implementing the functionality to delete a folder in Database Storage

--- app/public/src/controller/DropBoxController.js
+++ app/public/src/controller/DropBoxController.js
@@ -52,6 +52,61 @@ class DropBoxController {
 
     }
 
+    removeFolderTask(ref, name){
+
+        return new Promise((resolve, reject)=>{
+
+            let folderRef = this.getFirebaseRef(ref + '/' + name);
+
+            folderRef.on('value', snapshot => {
+
+                folderRef.off('value');
+
+                snapshot.forEach(item=>{
+
+                    let data = item.val();
+                    data.key = item.key;
+
+                    if (data.type === 'folder') {
+
+                        this.removeFolderTask(ref + '/' + name, data.name).then(()=>{
+
+                            resolve({
+                                fields:{
+                                    key: data.key
+                                }
+                            });
+
+                        }).catch(err=>{
+                            reject(err);
+                        });
+
+                    }else if (data.type) {
+
+                        this.removeFile(ref + '/' + name, data.name).then(()=>{
+
+                            resolve({
+                                fields:{
+                                    key: data.key
+                                }
+                            });
+
+                        }).catch(err=>{
+                            reject(err);
+                        });
+
+                    }
+
+                });
+
+                folderRef.remove();
+
+            });
+
+        });
+
+    }
+
     removeTask(){
 
         let promises =[];
@@ -63,27 +118,45 @@ class DropBoxController {
 
             promises.push(new Promise((resolve, reject)=>{
 
-               let fileRef = firebase.storage().ref(this.currentFolder.join('/')).child(file.name);
+                if (file.type === 'folder') {
 
-                fileRef.delete().then(()=>{
+                    this.removeFolderTask(this.currentFolder.join('/'), file.name).then(()=>{
 
-                    resolve({
-                        fields:{
-                            key
-                        }
+                        resolve({
+                            fields:{
+                                key
+                            }
+                        });
+        
                     });
 
-                }).catch(err=>{
+                } else if (file.type) {
 
-                    reject(err);
+                    this.removeFile(this.currentFolder.join('/'), file.name).then(()=>{
 
-                })
+                        resolve({
+                            fields:{
+                                key
+                            }
+                        });
+        
+
+                    });
+                }
 
             }));
 
         });
 
         return Promise.all(promises);
+
+    }
+
+    removeFile(ref, name){
+
+        let fileRef = firebase.storage().ref(ref).child(name);
+
+        return fileRef.delete();
 
     }
 
